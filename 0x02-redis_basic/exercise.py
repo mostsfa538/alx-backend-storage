a4939df9cc808279330d868cb8dd2f45542ae8ea
mostsfa538 <mostafa51moktar@gmail.com>
2024-08-14 14:55:21 +0300

    all

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -3,6 +3,51 @@
 import redis
 import uuid
 from typing import Union, Optional, Callable
+from functools import wraps
+
+
+def count_calls(method: Callable) -> Callable:
+    """ ..... """
+    key = method.__qualname__
+
+    @wraps(method)
+    def wrapper(self, *args, **kwargs):
+        self._redis.incr(key)
+
+        return method(self, *args, **kwargs)
+    return wrapper
+
+
+def call_history(method: Callable) -> Callable:
+    """ ..... """
+    in_key = f'{method.__qualname__}:inputs'
+    out_key = f'{method.__qualname__}:outputs'
+
+    @wraps(method)
+    def wrapper(self, *args, **kwargs):
+        self._redis.rpush(in_key, str(args))
+
+        res = method(self, *args, **kwargs)
+        self._redis.rpush(out_key, res)
+
+        return res
+    return wrapper
+
+
+def replay(method: Callable) -> None:
+    """ ..... """
+
+    key = method.__qualname__
+    cache = redis.Redis()
+    inputs_history = cache.lrange(f'{key}:inputs', 0, -1)
+    outputs_history = cache.lrange(f'{key}:outputs', 0, -1)
+
+    print(f'{key} was called {len(inputs_history)} times:')
+
+    for inputs, output in zip(inputs_history, outputs_history):
+        inputs_str = inputs.decode('utf-8')
+        output_str = output.decode('utf-8')
+        print(f'{key}(*{inputs_str}) -> {output_str}')
 
 
 class Cache:
@@ -13,6 +58,8 @@ class Cache:
         self._redis = redis.Redis()
         self._redis.flushdb()
 
+    @call_history
+    @count_calls
     def store(self, data: Union[str, bytes, int, float]) -> str:
         """ .... """
         key = str(uuid.uuid4())
