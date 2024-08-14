c91780525c5f34aa3a7cf3e15fcb09a9c193acff
mostsfa538 <mostafa51moktar@gmail.com>
2024-08-14 14:41:31 +0300

    task 0

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -11,10 +11,10 @@ class Cache:
     def __init__(self):
         """ .... """
         self._redis = redis.Redis()
-        self.redis.flushdb()
+        self._redis.flushdb()
 
     def store(self, data: Union[str, bytes, int, float]) -> str:
         """ .... """
         key = str(uuid.uuid4())
         self._redis.set(key, data)
-        return key
+        return (key)
