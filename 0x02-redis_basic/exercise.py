deeb5a5b1f91cbc1f8c711becf2b99b7cf1b7daa
mostsfa538 <mostafa51moktar@gmail.com>
2024-08-14 14:47:03 +0300

    task 1

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -2,7 +2,7 @@
 """ ..... """
 import redis
 import uuid
-from typing import Union
+from typing import Union, Optional, Callable
 
 
 class Cache:
@@ -18,3 +18,17 @@ class Cache:
         key = str(uuid.uuid4())
         self._redis.set(key, data)
         return (key)
+
+    def get(self, key: str, fn: Optional[Callable] = None) -> Union[str,
+                                                                    int,
+                                                                    None]:
+        """ .... """
+        data = self._redis.get(key)
+
+        if data is None:
+            return None
+
+        if fn is not None:
+            return fn(data)
+
+        return (data)
